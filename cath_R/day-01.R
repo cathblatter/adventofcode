0eb7553c091e5d03b0197148ee89450c3f23a459
Catherine Blatter <catherine.blatter@unibas.ch>
2020-12-03 17:17:31 +0100

    update

--- cath_R/day-01.R
+++ cath_R/day-01.R
@@ -1,18 +1,29 @@
 library(tidyverse)
 
-numbers <- read_csv("inputs/day-01", col_names = "num")
+numbers <- read_csv("cath_inputs/day-01", col_names = "num")
 
+# part 1
 numbers %>% 
   mutate(num2 = num) %>% 
   expand(num, num2) %>% 
   filter(num+num2 == 2020) %>% 
   summarise(product = num*num2) %>% unique()
 
-
+# short version
 prod(intersect(numbers, 2020-numbers))
 
 
-prod(intersect(numbers, 2020 - outer(numbers, numbers, FUN = "+")))
+
+# part 2
+numbers %>% 
+  mutate(num2 = num,
+         num3 = num) %>% 
+  expand(num, num2, num3) %>% 
+  filter(num+num2+num3 == 2020) %>% 
+  summarise(product = num*num2*num3) %>% unique()
+
+# 
+# prod(intersect(numbers, 2020-outer(numbers, numbers, "+")))
 
 
 
--- cath_R/day-02.R
+++ cath_R/day-02.R
@@ -1,33 +1,45 @@
 library(tidyverse)
 
-passwords <- read_csv("inputs/day-02b", col_names = "policy")
-
-# part 1
-passwords %>% 
+passwords <- 
+  read_csv("cath_inputs/day-02b", col_names = "policy") %>% 
   extract(col = policy, 
           into = c("min", "max", "letter", "password"), 
           regex = "(\\d+)-(\\d+) (.): (.*)", convert = T) %>% 
-  mutate(check_letter = map2_int(password, letter, str_count)) %>% 
-  mutate(check = case_when(check_letter >= min & check_letter <= max ~ TRUE, 
-                           TRUE ~ FALSE)) %>% 
+  mutate(ID = row_number())
+
+# part 1
+passwords %>% 
+  mutate(count_letter = map2_int(password, letter, str_count)) %>% 
+  mutate(check = case_when(count_letter >= min & count_letter <= max ~ TRUE, 
+                           TRUE ~ FALSE)) %>%
   summarise(sum = sum(check == TRUE))
 
 # part 2
 passwords %>% 
-  extract(col = policy, 
-          into = c("min", "max", "letter", "password"), 
-          regex = "(\\d+)-(\\d+) (.): (.*)", convert = T) %>% 
-  mutate(check2 = map2(password, letter, str_locate_all), 
-         check3 = simplify_all(check2),
+  mutate(mini = str_sub(password, min, min),
+         maxi = str_sub(password, max, max),
+         check = case_when(mini == letter & maxi != letter ~ TRUE, 
+                           mini != letter & maxi == letter ~ TRUE, 
+                           TRUE ~ FALSE)) %>% 
+  summarise(sum = sum(check == TRUE))
+  
+passwords %>% 
+  mutate(check3 = simplify_all(map2(password, letter, str_locate_all)), 
          checkmin2 = case_when(str_detect(check3, as.character(min)) ~ 1, 
                                TRUE ~ 0),
          checkmax2 = case_when(str_detect(check3, as.character(max)) ~ 1, 
                                TRUE ~ 0),
-         total2 = map2_dbl(checkmin2, checkmax2, sum)) %>% filter(total2 == 1) %>% view()
-         checkmin = if_else(str_detect(check3, as.character(min)), TRUE, FALSE),
-         checkmax = if_else(str_detect(check3, as.character(max)), TRUE, FALSE),
-         total = pmap_dbl(list(checkmin, checkmax), sum)) %>% 
-  filter(total == 1)
+         total2 = map2_dbl(checkmin2, checkmax2, sum)) %>% 
+  filter(total2 == 1) %>% pull(ID)
+
+
+setdiff(right, wrong)
+
+# 
+#          checkmin = if_else(str_detect(check3, as.character(min)), TRUE, FALSE),
+#          checkmax = if_else(str_detect(check3, as.character(max)), TRUE, FALSE),
+#          total = pmap_dbl(list(checkmin, checkmax), sum)) %>% 
+#   filter(total == 1)
 
 
 # 1-3 a: abcde is valid: position 1 contains a and position 3 does not.
