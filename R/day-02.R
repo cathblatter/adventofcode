821da94fc1c2b8af285935e3604e4a73fdf9b75f
Catherine Blatter <catherine.blatter@unibas.ch>
2020-12-03 08:22:27 +0100

    update

--- R/day-02.R
+++ R/day-02.R
@@ -1,7 +1,8 @@
 library(tidyverse)
 
-passwords <- read_csv("inputs/day-02", col_names = "policy")
+passwords <- read_csv("inputs/day-02b", col_names = "policy")
 
+# part 1
 passwords %>% 
   extract(col = policy, 
           into = c("min", "max", "letter", "password"), 
@@ -11,16 +12,35 @@ passwords %>%
                            TRUE ~ FALSE)) %>% 
   summarise(sum = sum(check == TRUE))
 
+# part 2
 passwords %>% 
   extract(col = policy, 
           into = c("min", "max", "letter", "password"), 
           regex = "(\\d+)-(\\d+) (.): (.*)", convert = T) %>% 
-  mutate(check2 = map2(password, letter, str_locate_all)) %>%view()
-  hoist(., .col = start, "check2") %>% view()
-# , 
-         # check3 = map2_lgl(min, check2, .x %in% .y))
+  mutate(check2 = map2(password, letter, str_locate_all), 
+         check3 = simplify_all(check2),
+         checkmin2 = case_when(str_detect(check3, as.character(min)) ~ 1, 
+                               TRUE ~ 0),
+         checkmax2 = case_when(str_detect(check3, as.character(max)) ~ 1, 
+                               TRUE ~ 0),
+         total2 = map2_dbl(checkmin2, checkmax2, sum)) %>% filter(total2 == 1) %>% view()
+         checkmin = if_else(str_detect(check3, as.character(min)), TRUE, FALSE),
+         checkmax = if_else(str_detect(check3, as.character(max)), TRUE, FALSE),
+         total = pmap_dbl(list(checkmin, checkmax), sum)) %>% 
+  filter(total == 1)
 
 
-me <- "abddbbbbeb"
+# 1-3 a: abcde is valid: position 1 contains a and position 3 does not.
+# 1-3 b: cdefg is invalid: neither position 1 nor position 3 contains b.
+# 2-9 c: ccccccccc
 
-str_locate_all(me, "b") %>% simplify() [[1]][,1]
+tibble(min = c(1, 1, 3), 
+             max = c(3, 3, 9),
+             letter = c("a", "b", "c"),
+             password = c("abcde","cdefg", "ccccccccc")) %>% 
+  mutate(check2 = map2(password, letter, str_locate_all),
+         check3 = simplify_all(check2),
+         checkmin = if_else(str_detect(check3, as.character(min)), TRUE, FALSE),
+         checkmax = if_else(str_detect(check3, as.character(max)), TRUE, FALSE),
+         total = pmap_dbl(list(checkmin, checkmax), sum)) %>% 
+  filter(total == 1) %>% nrow()
