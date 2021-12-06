6a8e053dfb6500c4e1732f6785ef35ce0c5af964
Catherine Blatter <catherine.blatter@unibas.ch>
2021-12-06 13:39:56 +0100

    day3....

--- 2021/day03.R
+++ 2021/day03.R
@@ -30,4 +30,67 @@ prod(gamma, epsilon)
 
 # part B ------------------------------------------------------------------
 
+day3_num <- 
+  str_split_fixed(day3$x, "", 12) %>% 
+  as_tibble() %>% 
+  mutate(across(.cols = everything(), 
+                .fns = ~as.numeric(.)))
 
+
+
+# code from Hannes Oberreiter - I could not solve this one 
+# but took his code to work through the solution
+
+# Part 1
+fHelper <- function(x, t = "min") {
+  if (t == "min") {
+    r <- names(table(x))[which.min(table(x))]
+  } else {
+    r <- names(table(x))[which.max(table(x))]
+  }
+  as.integer(r)
+}
+
+toBinary <- function(x) {
+  as.character(x) %>%
+    paste0(collapse = "") %>%
+    strtoi(base = 2)
+}
+
+
+fHelper2 <- function(x, t = "min") {
+  zero <- sum(x == 0)
+  one <- sum(x == 1)
+  if (t == "min") {
+    binary <- 0
+    if (one < zero) {
+      binary <- 1
+    }
+  } else {
+    binary <- 1
+    if (zero > one) {
+      binary <- 0
+    }
+  }
+  binary
+}
+
+
+fRec <- function(x, pos = 1, t = "max") {
+  if (nrow(x) == 1) {
+    return(x)
+  }
+  r <- x[, pos] %>%
+    unlist() %>%
+    fHelper2(., t = t)
+  fRec(x[x[, pos] == r, ], pos + 1, t = t)
+}
+
+oxygen <- fRec(day3_num, t = "max") %>%
+  toBinary()
+
+co2 <- fRec(day3_num, t = "min") %>%
+  toBinary()
+
+
+oxygen * co2
