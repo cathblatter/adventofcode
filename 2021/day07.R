546c88d79fdfcbbf37f3fd8c98b2be89d1f65500
Catherine Blatter <catherine.blatter@unibas.ch>
2021-12-07 13:12:29 +0100

    day07 part B

--- 2021/day07.R
+++ 2021/day07.R
@@ -1,9 +1,8 @@
 # Day 7
 
+library(tidyverse)
+
 day7 <- readr::parse_number(unlist(stringr::str_split(readLines("2021/inputs/day07.txt"), ",")))
-
-
-
 
 # Test case ---------------------------------------------------------------
 
@@ -13,14 +12,31 @@ test <- c(16,1,2,0,4,2,7,1,2,14)
 sum(abs(test - median(test)))
 
 
+# using mean for part 2
+gg <- abs(test - ceiling(mean(test)))
+gg <- data.frame(gg)
+
+gg %>% 
+  rowwise() %>% 
+  mutate(test = sum(1:gg)) %>% 
+  ungroup() %>% 
+  summarise(sum = sum(test))
+
+
 
 # Part A ------------------------------------------------------------------
 
 sum(abs(day7 - median(day7)))
 
 
-
-
 # Part B ------------------------------------------------------------------
 
+# using floor mean for part 2 (I started with ceiling and failed, so witched to floor)
+gg <- abs(day7 - floor(mean(day7)))
+gg <- data.frame(gg)
 
+gg %>% 
+  rowwise() %>% 
+  mutate(test = sum(1:gg)) %>% 
+  ungroup() %>% 
+  summarise(sum = sum(test))
