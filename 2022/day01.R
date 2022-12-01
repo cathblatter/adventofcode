7108653715609b33b7a13a23ff8738aea7556883
Catherine Blatter <catherine.blatter@unibas.ch>
2022-12-01 16:26:28 +0100

    day01  🤓

---
+++ 2022/day01.R
@@ -0,0 +1,43 @@
+# get input
+source("advent_input.R")
+
+# load pkgs
+library(tidyverse)
+
+
+# solve problem
+input <- advent_input(day = 1, year = 2022)
+
+# part 1
+input %>% 
+  mutate(grp = cumsum(is.na(x))) %>% 
+  filter(!is.na(x)) %>% 
+  rename(n_cal = x) %>% 
+  group_by(grp) %>% 
+  summarise(n = sum(n_cal)) %>% arrange(-n)
+
+
+# neater option for part 1
+input %>% 
+  mutate(grp = cumsum(is.na(x))) %>% 
+  drop_na(x) %>% 
+  group_by(grp) %>% 
+  tally(., wt = x, sort = T)
+
+
+
+
+# part 2
+input %>% 
+  mutate(grp = cumsum(is.na(x))) %>% 
+  filter(!is.na(x)) %>% 
+  rename(n_cal = x) %>% 
+  group_by(grp) %>% 
+  summarise(n = sum(n_cal)) %>% 
+  ungroup() %>% 
+  arrange(-n) %>% 
+  slice_head(n=3) %>% 
+  tally(n)
+
+
+
